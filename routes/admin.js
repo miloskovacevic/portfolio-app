d98c7897ff91690408ed63020129df950342514d
Milos <miloscpt@gmail.com>
2016-01-11 15:28:08 +0100

    Add project:done, todo: edit, delete

--- routes/admin.js
+++ routes/admin.js
@@ -21,4 +21,63 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/new', function(req, res){
+    res.render('new');
+});
+
+router.post('/new', function(req, res){
+    var title        = req.body.title;
+    var description  = req.body.description;
+    var service      = req.body.service;
+    var client       = req.body.client;
+    var projectdate  = req.body.projectdate;
+
+    //check Image
+    if(req.file.singleInputFileName){
+        var projectImageOriginalName  = req.files.singleInputFileName.originalname;
+        var projectImageName          = req.files.singleInputFileName.name;
+        var projectImageMime          = req.files.singleInputFileName.mimetype;
+        var projectImagePath          = req.files.singleInputFileName.path;
+        var projectImageExt           = req.files.singleInputFileName.extension;
+        var projectImageSize          = req.files.singleInputFileName.size;
+    }else {
+        var projectImageName = 'noimage.jpg';
+    }
+
+    req.checkBody('title', 'Title field is required').notEmpty();
+    req.checkBody('service', 'Service field is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('new', {
+            errors: errors,
+            title: title,
+            description: description,
+            servicce: service,
+            client: client,
+            projectdate: projectdate
+        });
+    }else {
+        var project = {
+            title: title,
+            description: description,
+            service: service,
+            client: client,
+            date: projectdate,
+            image: projectImageName
+        };
+        var query = connection.query('INSERT INTO projects SET ?', project, function (err, result) {
+            // project inserted
+            console.log('project inserted!');
+        });
+
+        req.flash('success', 'Project added');
+
+        res.location('/admin');
+        res.redirect('/admin');
+
+    }
+});
+
 module.exports = router;
